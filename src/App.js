6f601a0cc51d374ce399e83e089f99ea626c5788
strtw <souporstu@gmail.com>
2019-06-07 17:40:17 -0700

    added CodePenCard component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './components/CodePenCard.js';
+import Button from '@material-ui/core/Button';
+import CodePenCard from './components/CodePenCard.js';
+
+const data = {
+  height:200,
+  width:400,
+  titles:"KEoLaV",
+  penUrl:"//codepen.io/stw/embed/KEoLaV/?height=228&theme-id=0&default-tab=html,result"
+}
 
 function App() {
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+       
       </header>
+      <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+    <CodePenCard height={data.height} width={data.width} title={data.titles} src={data.penUrl}/>
+  
     </div>
   );
 }
