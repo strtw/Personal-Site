0705e565b6a5d5f5f0c4c2ada98ac86c5d77fc67
StuWood <stuw@convergedirect.com>
2019-06-21 15:13:40 -0700

    put test data in js file, added map function to app.js to render componenets from data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import './components/CodePenCard.js';
+import summaryData from './summaryData.js'
 import Button from '@material-ui/core/Button';
 import CodePenCard from './components/CodePenCard.js';
 import TextSummary from './components/TextSummary.js'
@@ -13,13 +14,14 @@ const data = {
   penUrl:"//codepen.io/stw/embed/KEoLaV/?height=228&theme-id=0&default-tab=html,result"
 }
 
-const summaryData = {
-  title: "This is the title",
-  summary: "This is the summary"
-}
+
 
 function App() {
-  console.log(data);
+  const summaryComponents = summaryData.map(project=> {
+    return (
+      <TextSummary title={project.title} summary={project.summary}/>
+    )
+  })
   return (
     <div className="App">
       <header>
@@ -29,7 +31,7 @@ function App() {
       Hello World
     </Button>
     <CodePenCard height={data.height} width={data.width} title={data.titles} src={data.penUrl}/>
-    <TextSummary title={summaryData.title} summary={summaryData.summary}/>
+    {summaryComponents}
   
     </div>
   );
---
+++ src/summaryData.js
@@ -0,0 +1,16 @@
+const summaryData = [
+    {
+      id: 1,
+      title: "This is the title",
+      summary: "This is the summary",
+      tags:[1,2,3]
+    },
+    {
+      id: 1,
+      title: "This is the title",
+      summary: "This is the summary",
+      tags:[1,2,3]
+    }
+  ]
+
+  export default summaryData;
