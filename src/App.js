69f28655d5706c5ba160043af4f62574856d3efd
StuWood <stuw@convergedirect.com>
2019-07-15 16:24:08 -0700

    added summary data to elasticlunr index, created function to search index

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Button from '@material-ui/core/Button';
 import CodePenCard from './components/CodePenCard.js';
 import TextSummary from './components/TextSummary.js';
 import Searchbar from './components/SearchBar.js';
+import { tsIndexSignature } from '@babel/types';
 
 
 const data = {
@@ -33,12 +34,53 @@ class App extends Component{
     this.setState({searchQuery:childQueryData})
   }
 
-filterProjects = (data) => {
-  console.log(data);
+filterProjects = (index,query) => {
+  let result = index.search(query);
+  return result;
+}
+
+
+loadProjectToSearchIndex = (data) =>{
+  var index = window.elasticlunr(function () {
+    this.addField('title');
+    this.addField('summary');
+    this.setRef('id');
+  });
+  data.forEach(function(project){
+    index.addDoc(project)
+  })
+  return index;
+}
+
+/*updateResultDisplay = (filteredArray, allProjects) =>{
+  
+  let result = [];
+  
+  allProject.forEach(function(project){
+    project.diplay = false; //set all projects to be hidden at beginning of search
+  })
+  
+  
+  filteredArray.forEach(function(project){
+   let id = project.id;
+  })
+
+  result.forEach(function(project){
+    
+  })
+}*/
+
+componentDidUpdate(){
+ let searchIndex = this.loadProjectToSearchIndex(summaryData.data);
+ let filteredProjects = this.filterProjects(searchIndex, this.state.searchQuery);
+console.log(filteredProjects);
+this.updateResultDisplay(filteredProjects,this.state.summaryData.data);
+
 }
 
   render(){
-    const summaryComponents = this.state.summaryData.map(project=> {
+    console.log(this.state.summaryData);
+    let summaryComponents = this.state.summaryData.data.map(project=> {
       return (
         <TextSummary display={project.display} key={project.id} title={project.title} summary={project.summary} repository={project.repository} demo={project.demo} tags={project.tags}/>
       )
@@ -52,7 +94,7 @@ filterProjects = (data) => {
         Hello World
       </Button>
      {/*} <CodePenCard height={data.height} width={data.width} title={data.titles} src={data.penUrl}/>*/}
-      <Searchbar getSearchQuery={this.getSearchQueryP}></Searchbar>
+      <Searchbar getSearchQuery={this.getSearchQueryP} projects={this.state.summaryData}></Searchbar>
       {summaryComponents}
     
       </div>
--- src/summaryData.js
+++ src/summaryData.js
@@ -1,22 +1,28 @@
-const summaryData = [
+import { yieldExpression } from "@babel/types";
+
+const summaryData = {data:[
     {
       id: 1,
       title: "This is the title",
-      summary: "This is the summary",
+      summary: "This is the cat",
       tags:[1,2,3],
       demo:'http://www.google.com',
       repository:'http://www.google.com', 
-      tags:['one','two','three']
+      tags:['one','two','three'],
+      display:true
     },
     {
       id: 2,
-      title: "This is the title",
-      summary: "This is the summary",
+      title: "This is the cat",
+      summary: "This is the dog",
       tags:[1,2,3],
       demo:'http://www.google.com',
       repository:'http://www.google.com',
-      tags:['one','two','three']
+      tags:['one','two','three'],
+      display:true
     }
-  ]
+  ]}
+
+  //const summaryData = '{"data":[{"id":1,"title":"This is the title","summary":"This is the cat","tags":["one","two","three"],"demo":"http://www.google.com","repository":"http://www.google.com"},{"id":2,"title":"This is the cat","summary":"This is the dog","tags":["one","two","three"],"demo":"http://www.google.com","repository":"http://www.google.com"}]}'
 
   export default summaryData;
