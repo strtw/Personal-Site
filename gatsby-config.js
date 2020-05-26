d16f0a682755f1991f7308359fb3085c580f7fa1
strtw <souporstu@gmail.com>
2020-05-26 15:42:30 -0700

    Update package.json to use gatsby-plugin-styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     options: {
       path: `${__dirname}/src/images/`,
     },
-  }, `gatsby-transformer-sharp`],
+  }, `gatsby-transformer-sharp`,`gatsby-plugin-styled-components`],
   siteMetadata: {
     title: "Stu Wood - Front-end Developer",
     description:
